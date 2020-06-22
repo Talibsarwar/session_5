4b2fb423fcd779f0496d856d6d5edb8eae85b3eb
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-06-22 04:15:20 +0000

    Update

---
+++ build/precache-manifest.8390f9a718a5a57808213e3a4d387e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9069431e905643a5551e9effb3a48282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "933b4a65303ca948b159",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c7a2a9aa53cb06a5c0a0",
+    "url": "/static/js/2.6a2d1347.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a2d1347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933b4a65303ca948b159",
+    "url": "/static/js/main.e1c7849f.chunk.js"
+  },
+  {
+    "revision": "a0b1d8ec14d03a05fa37",
+    "url": "/static/js/runtime-main.31bc8d14.js"
+  }
+]);
